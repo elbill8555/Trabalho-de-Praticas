5d63bfcedd720c3a371b717bd79fe31d937e54f3
elbill8555 <billadias8555@gmail.com>
2026-04-10 19:10:50 -0300

    aaa

--- trabalho_praticas/apps/backend/src/main.ts
+++ trabalho_praticas/apps/backend/src/main.ts
@@ -3,9 +3,22 @@ import { AppModule } from './app.module';
 
 let app;
 
+process.on('uncaughtException', (error) => {
+  console.error('[UNCAUGHT EXCEPTION]', error);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('[UNHANDLED REJECTION]', reason);
+});
+
 async function bootstrap() {
   if (!app) {
     try {
+      console.log('[BOOT 0] NODE_ENV=', process.env.NODE_ENV);
+      console.log('[BOOT 0] CLERK_SECRET_KEY defined=', !!process.env.CLERK_SECRET_KEY);
+      console.log('[BOOT 0] JWT_SECRET defined=', !!process.env.JWT_SECRET);
+      console.log('[BOOT 0] DATABASE_URL defined=', !!process.env.DATABASE_URL);
+
       console.log('[BOOT 1] Iniciando criação do NestFactory...');
       app = await NestFactory.create(AppModule);
       console.log('[BOOT 2] NestFactory criado com sucesso.');
